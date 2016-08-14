8d14f4fed01e46348ad6e7582f3debac5a00b0f4
Rosina Bignall <rosina@rosinabignall.com>
2016-08-14 00:47:19 -0700

    Revert "Only allow reloads once a minute to avoid the browser disabling the extension for too frequent reloads"
    This reverts commit 9b8b98679d00a7ba0894b2429502a06c9cd85fc5.

--- app/templates/scripts/chromereload.js
+++ app/templates/scripts/chromereload.js
@@ -8,12 +8,6 @@ const LIVERELOAD_HOST = 'localhost:';
 const LIVERELOAD_PORT = 35729;
 const connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
 
-var lastReload = false;
-
-chrome.runtime.onInstalled.addListener(function(details) {
-  lastReload = Date.now();
-});    
-
 connection.onerror = error => {
   console.log('reload connection got error:', error);
 };
@@ -22,13 +16,7 @@ connection.onmessage = e => {
   if (e.data) {
     const data = JSON.parse(e.data);
     if (data && data.command === 'reload') {
-      var currentTime = Date.now();
-      if (lastReload && currentTime - lastReload > 60000) {
-	// don't reload more than once a minute
-	chrome.runtime.reload();
-	chrome.developerPrivate.reload(chrome.runtime.id, 
-				       {failQuietly: true});
-      }
+      chrome.runtime.reload();
     }
   }
 };
@@ -36,12 +24,6 @@ connection.onmessage = e => {
 var LIVERELOAD_HOST = 'localhost:';
 var LIVERELOAD_PORT = 35729;
 var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
-
-var lastReload = false;
-
-chrome.runtime.onInstalled.addListener(function(details) {
-  lastReload = Date.now();
-});    
 
 connection.onerror = function (error) {
   console.log('reload connection got error:', error);
@@ -51,12 +33,7 @@ connection.onmessage = function (e) {
   if (e.data) {
     var data = JSON.parse(e.data);
     if (data && data.command === 'reload') {
-      if (lastReload && currentTime - lastReload > 60000) {
-	// don't reload more than once a minute
-	chrome.runtime.reload();
-	chrome.developerPrivate.reload(chrome.runtime.id, 
-				       {failQuietly: true});
-      }
+      chrome.runtime.reload();
     }
   }
 };
